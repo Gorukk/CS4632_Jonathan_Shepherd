77e9465e4da5b772d5eb5d5aa69223fb9ff16348
Gorukk <2002jshepherd@gmail.com>
2026-02-26 14:06:07 -0500

    Method to get infected population to dead population.

--- Dead.cs
+++ Dead.cs
@@ -3,17 +3,26 @@
 public class Dead
 {
 	int deadPop; //Amount of people that died from the disease
-	float deathrate; //Rate of infected individuals dying
+	float deathRate; //Rate of infected individuals dying
 
 	public Dead()
 	{
 		deadPop = 0;
-		deathrate = .25f;
+		deathRate = .25f;
 	}
 
 	public Dead(int deathRate)
 	{
 		deadPop = 0;
-		this.deathrate = deathRate;
+		this.deathRate = deathRate;
+	}
+
+	public int Death(int infPop)
+	{
+		int deltaPop = deathRate * infPop;
+
+		deadPop += deltaPop;
+
+		return deadPop;
 	}
 }
